09832fd8b867c6ea271b4b0a9f78bda899e3d431
daalopezma <danielalejandrol14@gmail.com>
2022-11-10 20:55:58 -0500

    Anadido rayo

--- js/logic.js
+++ js/logic.js
@@ -1,15 +1,21 @@
 var slider = document.getElementById("myRange");
 var output = document.getElementById("angulo");
+var incidenceAngle = document.getElementById("primeranguloincidencia");
+var inputRefractiveIndexAir = document.getElementById("refractiveIndexAir");
+var inputRefractiveIndexPrism = document.getElementById("refractiveIndexPrism");
 
 var angle = slider.value;
 output.innerHTML = angle; // Display the default slider value
+
+var refractiveIndexAir = inputRefractiveIndexAir.value;
+var refractiveIndexPrism = inputRefractiveIndexPrism.value;
 
 
 // Position of the incidence center
 let xIncidenceCenter = 400;
 let yIncidenceCenter = 400;
 let xExpectedIncidenceCenter = 400;
-let yExpectedIncidenceCenter = 500;
+let yExpectedIncidenceCenter = 400;
 
 // shape of the triangle
 let side = 200;
@@ -31,6 +37,8 @@ const sectionVerPrisma = document.getElementById('ver-prisma')
 const sectionDibujo = document.getElementById('dibujo')
 
 //Laser
+let distanceFromTheExpectedIncidenceCenterPhotoLaser = 300;
+
 function drawLaser(xcoordinate, ycoordinate){
     let laser = dibujo.getContext("2d");
     laser.fillStyle = "#a76bff";
@@ -41,17 +49,32 @@ function drawLaser(xcoordinate, ycoordinate){
 }
 
 function moveLaser(angle){
-    let xcoordinate = 400*Math.cos(angle*Math.PI/180+Math.PI/2)+xExpectedIncidenceCenter;
-    let ycoordinate = -400*Math.sin(angle*Math.PI/180+Math.PI/2)+yExpectedIncidenceCenter;
+    let xcoordinate = distanceFromTheExpectedIncidenceCenterPhotoLaser*Math.cos(angle+Math.PI/2)+xExpectedIncidenceCenter;
+    let ycoordinate = -distanceFromTheExpectedIncidenceCenterPhotoLaser*Math.sin(angle+Math.PI/2)+yExpectedIncidenceCenter;
     drawLaser(xcoordinate, ycoordinate);
+
     let m2 = (yExpectedIncidenceCenter - ycoordinate)/(xExpectedIncidenceCenter - xcoordinate);
     let b2 = yExpectedIncidenceCenter - m2*xExpectedIncidenceCenter;
     let xcorte = (b2-b1)/(m1-m2);
     let ycorte = m2*xcorte+b2; 
     let longitud = Math.hypot((xcorte-xcoordinate),(ycorte-ycoordinate));
-
-
+    let dotProductV1V2 = -(1+m1*m2);
+    let normV1 = Math.hypot(1,m1);
+    let normV2 = Math.hypot(1,m2);
+    let firstIncidenceAngle = Math.asin(dotProductV1V2/(normV1*normV2));
+    incidenceAngle.innerHTML = firstIncidenceAngle*180/Math.PI;
     drawRay(-angle, xcoordinate, ycoordinate, longitud);
+
+    let transmittedAngle = Math.asin(refractiveIndexAir*Math.sin(firstIncidenceAngle)/refractiveIndexPrism);
+    let sin3 = Math.sin(transmittedAngle+Math.PI/4);
+    let ycorte1 = point2y;
+    let longitud1 = (ycorte1-ycorte)/sin3;
+    drawRay(transmittedAngle-Math.PI/4, xcorte, ycorte, longitud1);
+    //normal
+    drawRay(3*Math.PI/4, xcorte, ycorte, 20);
+
+    
+
 }
 
 //Photodiode
@@ -66,10 +89,10 @@ function drawPhotodiode(xcoordinate, ycoordinate){
 }
 
 function movePhotodiode(angle){
-    let xcoordinate = -400*Math.cos(angle*Math.PI/180+Math.PI/2)+xExpectedIncidenceCenter;
-    let ycoordinate = -400*Math.sin(angle*Math.PI/180+Math.PI/2)+yExpectedIncidenceCenter;
+    let xcoordinate = -distanceFromTheExpectedIncidenceCenterPhotoLaser*Math.cos(angle+Math.PI/2)+xExpectedIncidenceCenter;
+    let ycoordinate = -distanceFromTheExpectedIncidenceCenterPhotoLaser*Math.sin(angle+Math.PI/2)+yExpectedIncidenceCenter;
     drawPhotodiode(xcoordinate, ycoordinate);
-    drawRay(angle, xcoordinate, ycoordinate, 400);
+    drawRay(angle, xcoordinate, ycoordinate, distanceFromTheExpectedIncidenceCenterPhotoLaser);
 }
 
 
@@ -78,8 +101,8 @@ slider.oninput = function() {
     output.innerHTML = this.value;
     angle = this.value;
     dibujo.getContext("2d").clearRect(0,0,dibujo.width,dibujo.height);
-    moveLaser(angle);
-    movePhotodiode(angle);
+    moveLaser(angle*Math.PI/180);
+    movePhotodiode(angle*Math.PI/180);
     drawPrisma();
 }
 
@@ -106,7 +129,7 @@ function drawRay(angulo, startx, starty, len ){
     const ray = dibujo.getContext("2d");
     ray.beginPath();
     ray.moveTo(startx, starty);
-    ray.lineTo(startx+len*Math.cos(angulo*Math.PI/180+Math.PI/2), starty+len*Math.sin(angulo*Math.PI/180+Math.PI/2));
+    ray.lineTo(startx+len*Math.cos(angulo+Math.PI/2), starty+len*Math.sin(angulo+Math.PI/2));
     ray.stroke();
 
 }
